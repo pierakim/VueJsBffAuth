5fcfa9a04def5e7de456b6665fa7726cb2176b08
pierakim <pierakim.lambda@gmail.com>
2022-07-10 11:48:20 +1000

    cleanup from vue template

--- src/VueJSClient/client-app/src/views/HomeView.vue
+++ src/VueJSClient/client-app/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Hello to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="Hello." />
   </div>
 </template>
 
